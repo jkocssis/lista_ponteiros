9dde86a3c0a430e6b4cf2c2e02cd3e8d6363e5bb
jkocssis <57385688+jkocssis@users.noreply.github.com>
2020-03-19 16:32:51 -0300

    final
    final

--- lista_exe6.c
+++ lista_exe6.c
@@ -1,38 +1,37 @@
 #include<stdio.h>
-void ordena(double vet[], int n);
+void minimo(double vet[], int n);
 void imprime(double v[],int n);
 double media(double vet[], int n, int *i);
 
 int main(int argc, char const *argv[])
 {
     int n=6,prox;
-    double v[]={0, 3.2 ,5,6,15,2 },m;//declara o vetor
-    imprime(v,n); //imprime o vetor ordenado
+    double v[]={0, 3.2 ,7,6,15,2 },m;//declara o vetor
+    
 
-    m = media(v,n,&prox); //m recebe a media
+    m = media(v,n,&prox); //m recebe a media, *i aponta para prox(valor mais proximo da media)
+    
+    imprime(v,n); //imprime o vetor 
 
     printf("A media para o vetor = %.2lf\n",m);//imprime a média
-    printf("O valor mais proximo da media = %d\n",prox);// imprime o valor do vetor mais proximo da media
+    printf("A posicao mais proxima da media = %d\n",prox);// imprime o valor do vetor mais proximo da media
     
     return 0;
 }
 
 
-void ordena(double vet[], int n){
-    //funcao ordena vetor
-    double aux;
-    int i, j;
+void minimo(double vet[], int n){
+    
+    double minimo=vet[0];
+    int i, j=0;
     for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            if (vet[i]<vet[j])
-            {
-                aux = vet[i];
-                vet[i] = vet[j];
-                vet[j] = aux;
-            }
-            
+        if (vet[i]<minimo)
+        {
+            j=i;
         }
+        
     }
+    printf("minimo=%d\n",j);
     
 }
 void imprime(double v[],int n){
@@ -43,40 +42,37 @@ void imprime(double v[],int n){
     }
     printf("\n");
 }
-double media(double vet[], int n, int *i){
-   double x=0,media;
-   
-   int j,mediana;
-   if (n%2==0)//verifica de mediana é par ou impar
-   {
-       mediana = n/2;
-   }else
-   {
-       mediana = (n+1)/2;// se mediana for impar soma mais 1
-   }    
-   
-   *i=vet[mediana];
-   
-   ordena(vet,n);//ordena o vetor em ordem crescente
 
-   for(j=0; j<n;j++){
+double media(double vet[], int n, int *i){
+   double x=0,media, resto[n];
+   
+   int j;
+    
+    for(j=0; j<n;j++){
        x = x +vet[j];
    }
     media = x/n;//calcula a média dos valores do vetor
+    for (j=0;j<n;j++){
+        if (media-vet[j]<0)
+        {
+            resto[j]=vet[j]-media;
+        }else
+        {
+            resto[j]=media-vet[j];
+        }
+    }
+    double min=resto[0];
 
-   for(j=0; j<n; j++){
-       if (vet[mediana]-media > vet[mediana+j]-media)//verifica qual o valor em torno da mediana esta mais próximo da média
-       {
-           *i=vet[mediana];
-       }else
-       {
-          *i=vet[mediana+j];
-       }
-       
-       
-   }
-   
-   
-   
+    for(j=0; j<n;j++){
+        if (resto[j]<min)
+        {
+            min=j;
+            
+        }
+        
+    }
+    *i=(int)min;
+
+     
    return media;
 }
